8552550b78b5e7e18989be3f9578b758821af127
Bpopson11 <Bpopson@gmail.com>
2016-02-23 16:23:48 -0800

    Initial file set up

---
+++ food/tests/CuisineTest.php
@@ -0,0 +1,27 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Cuisine.php";
+
+    $server = 'mysql:host=localhost;dbname=my_inventory_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+
+    class CuisineTest extends PHPUnit_Framework_TestCase
+    {
+
+        protected function tearDown()
+        {
+            Cuisine::deleteAll();
+        }
+
+
+    }
+
+?>
