d1a2b54ebeea44256d0c503d2761dd77bc0b8320
Afton Downey and Brianna Popson <aaike001@gmail.com>
2016-02-24 16:40:00 -0800

    add comments to code

--- app/app.php
+++ app/app.php
@@ -14,27 +14,35 @@
     'twig.path' => __DIR__.'/../views'
     ));
 
+    $app ['debug'] = true; //YOUR BEST FRIEND!!!!!!!!!!!
+
     use Symfony\Component\HttpFoundation\Request;
     Request::enableHttpMethodParameterOverride();
 
-
+    //not sure if you need the commented out $cuisine info. Index.html.twig won't load if it isn't commented out. Just FYI.
     $app->get("/", function() use ($app) {
-        $cuisine = new Cuisine($_GET['cuisine_type']);
-        $cuisine->save();
-        return $app['twig']->render('index.html.twig', array('cuisines' => Cuisine::getAll(), 'cuisine' => $cuisine));
+        // $cuisine = new Cuisine($_GET['cuisine_type']);
+        // $cuisine->save();
+        return $app['twig']->render('index.html.twig', array('cuisines' => Cuisine::getAll()));
     });
 
     $app->get("/restaurants", function() use ($app) {
         return $app['twig']->render('restaurants.html.twig', array('restaurants' => Restaurant::getAll()));
     });
 
+    //supposed to display all restaurants under a Cuisine type (breaks after submitting new restaurant)
     $app->post("/restaurants", function() use ($app) {
-        $restaurant = new Restaurant($_POST['name'], $_POST['website'], $_POST['phone_number'], $_POST['cuisine_id']);
+        $name = $_POST['name'];
+        $website = $_POST['website'];
+        $phone_number = $_POST['phone_number'];
+        $cusine_id = $_POST['cuisine_id'];
+        $restaurant = new Restaurant($name, $website, $phone_number, $id = null, $cuisine_id);
         $restaurant->save();
-        $cusine = Cuisine::findCuisine($cuisine_id);
-        return $app['twig']->render('restaurants.html.twig', array('restaurants' => Restaurant::getAll()));
+        $cuisine = Cuisine::find($cuisine_id);
+        return $app['twig']->render('restaurants.html.twig', array('cuisine' => $cuisine, 'restaurant' => Cuisine::getRestaurants()));
     });
 
+    //Everything below this point should work (it did last time we checked)
     $app->get("/cuisines", function() use ($app) {
         return $app['twig']->render('cuisines.html.twig', array('cuisines' => Cuisine::getAll()));
     });
@@ -44,8 +52,6 @@
         $cuisine->save();
         return $app['twig']->render('cuisines.html.twig', array('cuisines' => Cuisine::getAll(), 'cuisine' => $cuisine));
     });
-
-        $app ['debug'] = true;
 
     $app->get("/cuisines/{id}", function($id) use ($app) {
         $cuisine = Cuisine::findCuisine($id);
